95b7c864b3b4d6fdca05c58049814fc5fd483fe2
mariaangelamagtoto <angela.magtoto@mvp.net.ph>
2018-03-16 13:20:20 +0800

    Sample fileViewer

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,7 +2,7 @@ import {Component} from '@angular/core';
 import {NavController, Platform, AlertController} from 'ionic-angular';
 import {Transfer, TransferObject} from '@ionic-native/transfer';
 import {File} from '@ionic-native/file';
-
+import { FileOpener } from '@ionic-native/file-opener';
 declare var cordova: any;
 
 @Component({
@@ -15,7 +15,7 @@ export class HomePage {
 
   storageDirectory: string = '';
 
-  constructor(public navCtrl: NavController, public platform: Platform, private transfer: Transfer, private file: File, public alertCtrl: AlertController) {
+  constructor(private fileOpener: FileOpener, public navCtrl: NavController, public platform: Platform, private transfer: Transfer, private file: File, public alertCtrl: AlertController) {
     this.platform.ready().then(() => {
       // make sure this is on a device, not an emulation (e.g. chrome tools device mode)
       if(!this.platform.is('cordova')) {
@@ -44,15 +44,15 @@ export class HomePage {
       const imageLocation = `${cordova.file.applicationDirectory}www/assets/img/${image}`;
 
       fileTransfer.download(imageLocation, this.storageDirectory + image).then((entry) => {
-
         const alertSuccess = this.alertCtrl.create({
           title: `Download Succeeded!`,
           subTitle: `${image} was successfully downloaded to: ${entry.toURL()}`,
           buttons: ['Ok']
         });
-
         alertSuccess.present();
-
+        this.fileOpener.open(entry.toURL(), 'application/pdf')
+        .then(() => console.log('File is opened'))
+        .catch(e => console.log('Error openening file', e));
       }, (error) => {
 
         const alertFailure = this.alertCtrl.create({
